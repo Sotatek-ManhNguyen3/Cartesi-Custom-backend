a90f312ca7ebb439b5e0aa75581c700e3d582f69
Tien Manh <manh.nguyen3@sotatek.com>
2022-04-14 14:51:20 +0700

    Update the library

--- trustedtrip.py
+++ trustedtrip.py
@@ -1,22 +1,49 @@
 import flask
-from flask import request, jsonify
+from flask import request
 import requests
 import json
-import random
+from lib import picket
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
 def isInTheTollZone(gps_data):
-    print("Latitude: " + gps_data[2])
-    print("Longtitude: " + gps_data[4])
-    return random.randint(0, 1) == 1
+    latitude = float(gps_data[2][:2]) + float(gps_data[2][2:]) / 60
+    longitude = float(gps_data[4][:2]) + float(gps_data[4][2:]) / 60
+    print("Latitude: " + str(latitude))
+    print("Longitude: " + str(longitude))
+
+    f = open('Airport_Runway_Protection_Zone_and_Inner_Safety_Zone.geojson')
+    data = json.load(f)
+
+    for zone in data['features']:
+        if zone['properties']['ZONE_TYPE'] != "Runway Protection Zone":
+            continue
+
+        for inner_zone in zone['geometry']['coordinates']:
+            if len(inner_zone) < 3:
+                continue
+
+            fence = create_fence(inner_zone)
+            if fence.check_point((longitude, latitude)):
+                return True
+
+    return False
+
+
+def create_fence(coordinates):
+    fence = picket.Fence()
+
+    for each_pair in coordinates:
+        fence.add_point((each_pair[0], each_pair[1]))
+
+    return fence
 
 
 @app.route('/advance', methods=['POST'])
 def advance():
-    dispatcher_url = 'http://192.168.31.148:5004'
+    dispatcher_url = 'http://10.2.14.167:5004'
     body = request.get_json("metadata")
     print(f"Received advance request body {body}")
 
@@ -41,7 +68,7 @@ def advance():
     print("Finishing")
     response = requests.post(dispatcher_url + "/finish", json={"status": "accept"})
     print(f"Received finish status {response.status_code}")
-    return "abc", 202
+    return "", 202
 
 
 @app.route('/inspect', methods=['GET'])
